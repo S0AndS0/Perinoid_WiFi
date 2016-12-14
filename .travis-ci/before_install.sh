a161bfe5adae158a828fa09cac576e1644270da6
S0AndS0 <strangerthanbland@gmail.com>
2016-12-14 03:03:49 +0000

    Trying different approch with depends

--- .travis-ci/before_install.sh
+++ .travis-ci/before_install.sh
@@ -15,8 +15,10 @@ if [ -e "${Var_main_script_name}" ]; then
 	./${Var_main_script_name} --version
 	_exit_status=$?
 	Func_check_exit_status "${_exit_status}" "${Var_main_script_name} --version"
-	echo "# ${Var_script_name} running: ./${Var_main_script_name} --debug-level='9' --apt-check-depends-yn=\"yes\" --apt-depends-list=\"${Var_main_script_apt_get_depends_list}\""
-	./${Var_main_script_name} --debug-level='9' --apt-check-depends-yn="yes" --apt-depends-list="${Var_main_script_apt_get_depends_list}"
+#	echo "# ${Var_script_name} running: sudo ./${Var_main_script_name} --debug-level='9' --apt-check-depends-yn=\"yes\" --apt-depends-list=\"${Var_main_script_apt_get_depends_list}\""
+#	sudo ./${Var_main_script_name} --debug-level='9' --apt-check-depends-yn="yes" --apt-depends-list="${Var_main_script_apt_get_depends_list}"
+	echo "# ${Var_script_name} running: sudo apt-get update -qqq && sudo apt-get install ${Var_main_script_apt_get_depends_list}"
+	sudo apt-get update -qqq && sudo apt-get install ${Var_main_script_apt_get_depends_list}
 	_exit_status=$?
 	Func_check_exit_status "${_exit_status}" "./${Var_main_script_name} --debug-level='9' --apt-check-depends-yn=\"yes\" --apt-depends-list=\"${Var_main_script_apt_get_depends_list}\""
 else
--- .travis-ci/script_configure_clients_only.sh
+++ .travis-ci/script_configure_clients_only.sh
@@ -5,8 +5,8 @@ source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables"
 echo "# ${Var_script_name} started at: $(date -u +%s)"
 if [ -e "${Var_main_script_name}" ]; then
-	echo "# ${Var_script_name} running: ./${Var_main_script_name} --debug-level='9' --ovpnc-config-yn='yes' --ovpnc-pass-yn='yes' --ovpnc-host-ports=\"${Var_external_ip}:${Var_listen_port}\" --ovpnc-names=\"${Var_client_users_list}\""
-	./${Var_main_script_name} --debug-level='9' --ovpnc-config-yn='yes' --ovpnc-pass-yn='yes' --ovpnc-host-ports="${Var_external_ip}:${Var_listen_port}" --ovpnc-names="${Var_client_users_list}"
+	echo "# ${Var_script_name} running: sudo ./${Var_main_script_name} --debug-level='9' --ovpnc-config-yn='yes' --ovpnc-pass-yn='yes' --ovpnc-host-ports=\"${Var_external_ip}:${Var_listen_port}\" --ovpnc-names=\"${Var_client_users_list}\""
+	sudo ./${Var_main_script_name} --debug-level='9' --ovpnc-config-yn='yes' --ovpnc-pass-yn='yes' --ovpnc-host-ports="${Var_external_ip}:${Var_listen_port}" --ovpnc-names="${Var_client_users_list}"
 	_exit_status=$?
 	Func_check_exit_status "${_exit_status}" "./${Var_main_script_name} --debug-level='9' --ovpnc-config-yn='yes' --ovpnc-pass-yn='yes' --ovpnc-host-ports=\"${Var_external_ip}:${Var_listen_port}\" --ovpnc-names=\"${Var_client_users_list}\""
 else
