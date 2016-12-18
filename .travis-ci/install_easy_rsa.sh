8aaef1b767b7944f24c3792c89b5b032433eb484
S0AndS0 <strangerthanbland@gmail.com>
2016-12-18 22:25:50 +0000

    Trying manual install from deb file before getting any cleverer with auto builds

--- .travis-ci/install_easy_rsa.sh
+++ .travis-ci/install_easy_rsa.sh
@@ -4,18 +4,11 @@ export Var_script_name="${0##*/}"
 source "${Var_script_dir}/lib/functions.sh"
 Func_source_file "${Var_script_dir}/lib/variables"
 echo "# ${Var_script_name} started at: $(date -u +%s)"
-Var_source_string='deb http://ftp.us.debian.org/debian unstable main contrib non-free'
-echo "# ${Var_script_name} running: echo \"${Var_source_string}\" | sudo tee -a /etc/apt/sources.list.d/custom_testing.list"
-echo "${Var_source_string}" | sudo tee -a /etc/apt/sources.list.d/custom_testing.list
+Var_deb_url="http://ftp.us.debian.org/debian/pool/main/e/easy-rsa/easy-rsa_2.2.2-1_all.deb"
+Var_deb_file="${Var_deb_url##*/}"
+echo "# ${Var_script_name} running: wget \"${Var_deb_url}\" -qO \"${Var_deb_file}\" && sudo dpkg -i \"${Var_deb_file}\""
+wget "${Var_deb_url}" -qO "${Var_deb_file}" && sudo dpkg -i "${Var_deb_file}"
 _exit_status=$?
-Func_check_exit_status "${_exit_status}" "echo \"${Var_source_string}\" | sudo tee -a /etc/apt/sources.list.d/custom_testing.list"
-echo "# ${Var_script_name} running: sudo apt-get update 2> /tmp/keymissing; for key in \$(grep \"NO_PUBKEY\" /tmp/keymissing |sed \"s/.*NO_PUBKEY //\"); do echo -e \"nProcessing key: \$key\"; gpg --keyserver keys.gnupg.net --recv $key && gpg --export --armor \$key | sudo apt-key add -; done"
-sudo apt-get update 2> /tmp/keymissing; for key in $(grep "NO_PUBKEY" /tmp/keymissing |sed "s/.*NO_PUBKEY //"); do echo -e "nProcessing key: $key"; gpg --keyserver keys.gnupg.net --recv $key && gpg --export --armor $key | sudo apt-key add -; done
-_exit_status=$?
-Func_check_exit_status "${_exit_status}" "sudo apt-get update 2> /tmp/keymissing; for key in \$(grep \"NO_PUBKEY\" /tmp/keymissing |sed \"s/.*NO_PUBKEY //\"); do echo -e \"nProcessing key: \$key\"; gpg --keyserver keys.gnupg.net --recv $key && gpg --export --armor \$key | sudo apt-key add -; done"
-echo "# ${Var_script_name} running: sudo apt-get update && sudo apt-get install easy-rsa"
-sudo apt-get update && sudo apt-get install easy-rsa
-_exit_status=$?
-Func_check_exit_status "${_exit_status}" "sudo apt-get update && sudo apt-get install easy-rsa"
+Func_check_exit_status "${_exit_status}" "wget \"${Var_deb_url}\" -qO \"${Var_deb_file}\" && sudo dpkg -i \"${Var_deb_file}\""
 echo "# ${Var_script_name} reports: all checks passed"
 echo "# ${Var_script_name} finished at: $(date -u +%s)"
